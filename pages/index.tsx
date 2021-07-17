42cb2e38abfa14d3cdcc71aa270f678b65985851
DenpaGhost <az.kinosaki@gmail.com>
2021-07-17 21:06:56 +0900

    index (Early Access)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,15 @@
+import Link from "next/link";
+
 const index = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div>
+        <h1>Discord Superchater (Ealry Access)</h1>
+        <Link href="/oauth/discord/redirect" replace>
+          <a>ログイン</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default index;
